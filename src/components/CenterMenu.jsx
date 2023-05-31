4945be316eccf4ed9bfe352f43cd0ef25b5357c9
mariaemf <mariaeduardamartinsf@gmail.com>
2023-05-31 14:57:46 -0300

    realizaçao de novas incrementacaoes de css do tailwind

--- src/components/CenterMenu.jsx
+++ src/components/CenterMenu.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function CenterMenu() {
   return (
     <div className="menu flex">
-      <ul>
+      <ul className="flex w-[100%] justify-between">
         <li>Home</li>
         <li>About</li>
         <li>Preformer</li>
